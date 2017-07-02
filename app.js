3a9723d9f7bb5bd8389f88e761953e321f31d758
kvin1087 <kaustubh87@gmail.com>
2017-07-02 17:47:57 -0500

    Removed default user routes

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', index.show);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
