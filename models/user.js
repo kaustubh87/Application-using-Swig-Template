79180f431e47d7842dd56f93d547216ae49cf02d
kvin1087 <kaustubh87@gmail.com>
2017-07-04 21:34:16 -0500

    Created model using sequelize

---
+++ models/user.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = function(sequelize, DataTypes){
+    var User = sequelize.define('User', {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING
+    },{
+        classMethods: {
+            associate: function(models){
+
+            }
+        }
+    });
+}
